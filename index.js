4203591b85a9c18b91420d41ac8b39af1d5ca9b4
shaad72345 <shaad72345@gmail.com>
2023-06-28 01:16:11 +0530

    cors added

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require("express");
-
+var cors = require('cors');
 const {connection} = require("./dbConfig/config")
 const {todoRouter} = require("./routes/todo.routes");
 const{userRouter}= require("./routes/user.routes")
 const app = express();
 
+app.use(cors());
 app.use(express.json())
+
 app.get("/", (req,res)=>{
 res.send("Home")
 })
